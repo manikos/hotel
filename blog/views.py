3cf7b927f509133d93ff4bea19f299dd7b1502e0
manikos <mavrakis.n@gmail.com>
2016-12-09 19:46:18 +0200

    Support for the url /users/me/ to return user based on the given JWT

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework_jwt.utils import jwt_decode_handler
 
 from .serializers import PostSerializer, UserSerializer
+from hotel_start.settings import JWT_AUTH
 
 
 def homepage(request):
@@ -25,13 +26,15 @@ class UserDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
     authentication_classes = (JSONWebTokenAuthentication, )
     permission_classes = (permissions.IsAuthenticated, )
     lookup_field = 'username'
-    queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get(self.lookup_field) == 'me':
+            token = self.request.META.get('HTTP_AUTHORIZATION')
+            if token and JWT_AUTH.get('JWT_AUTH_HEADER_PREFIX') in token:
+                token = token.strip(JWT_AUTH.get('JWT_AUTH_HEADER_PREFIX') + ' ')
+                self.kwargs[self.lookup_field] = jwt_decode_handler(token).get(self.lookup_field)
+        return User.objects.all()
+
     def get(self, request, *args, **kwargs):
-        # if 'me' in kwargs:
-        #     token = request.META.get('HTTP_AUTHORIZATION')
-        #     if token and 'JWT' in token:
-        #         token = token.strip('JWT ')
-        #         kwargs['username'] = jwt_decode_handler(token).get('username')
         return self.retrieve(request, *args, **kwargs)
